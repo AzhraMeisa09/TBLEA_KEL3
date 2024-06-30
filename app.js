27098fe829df8026777037af13b27f81bb9f4469
umarazzam10 <umaraazzam10@gmail.com>
2024-06-30 15:07:28 +0700

    baru set up

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('views', [
+  path.join(__dirname, 'views/Mahasiswa'),
+  path.join(__dirname, 'views/Dosen'),
+  path.join(__dirname, 'views'),
+]);
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'views')));
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
